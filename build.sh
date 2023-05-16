5a99aa6d2cdd8315032484de05cd46cb39e4f717
Henry Darnell <hjdarnel@users.noreply.github.com>
2023-05-16 18:47:17 -0500

    Remove --also-windows flag

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ in_path=$(realpath .)/origin
 out_path=$(realpath .)/build
 
 mkdir -p $out_path
-docker run --rm -v $in_path:/in -v $out_path:/out nerdfonts/patcher -c --also-windows
-docker run --rm -v $in_path:/in -v $out_path:/out nerdfonts/patcher -c --also-windows --mono
+docker run --rm -v $in_path:/in -v $out_path:/out nerdfonts/patcher -c 
+docker run --rm -v $in_path:/in -v $out_path:/out nerdfonts/patcher -c --mono
